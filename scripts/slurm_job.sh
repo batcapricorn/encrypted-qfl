065dc9b8b3f2f5af4ea7c673b4bd9d76f87d8857
lukasboehm97 <lukasboehm97@web.de>
2025-04-28 11:16:50 +0200

    refactor pytorch models

--- scripts/slurm_job.sh
+++ scripts/slurm_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --gpus=v100:4
-#SBATCH --cpus-per-task=16
-#SBATCH --partition=clara
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=48
+#SBATCH --partition=barnard
 #SBATCH --time=2-00:00:00
 #SBATCH --job-name=fl_simulation      # Job name
 #SBATCH --mem=200G                     # Total memory
@@ -16,17 +16,12 @@ if [[ "$1" == "--he" ]]; then
     HE_FLAG="--he"
 fi
 
-module load CUDA/12.6.0
+module load release/24.10
 module load Anaconda3/2024.02-1
-source activate base
-pip install flwr==1.5.0 tqdm numpy pennylane "ray>=2.3.0" matplotlib pillow scikit-learn seaborn pandas opacus pyyaml tenseal psrecord yq wandb
-pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+source .venv/bin/activate
+#pip install flwr==1.5.0 tqdm numpy pennylane "ray>=2.3.0" matplotlib pillow scikit-learn seaborn pandas opacus pyyaml tenseal psrecord yq wandb
+#pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 
-if [ ! -d "/dev/shm/data" ]; then
-        echo "Copying dataset to /dev/shm/..."
-        cp -r ../data/ /dev/shm/
-else
-        echo "Dataset already exists in /dev/shm/, skipping copy."
-fi
+wandb login
 
 ./scripts/benchmark.sh $MODEL_TYPE $HE_FLAG
